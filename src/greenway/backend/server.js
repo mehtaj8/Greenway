4a62ddf12b03b872df25865f2cbffc0d192a7925
BilalShakh <bilalzaki92@gmail.com>
2022-11-13 01:49:39 -0500

    Created backend for mongodb and related logic for fetching elevation data

---
+++ src/greenway/backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const dbRouter = require("./routes/database");
+app.use("/db", dbRouter);
+
+app.listen(3001, () => {
+    console.log("Server started on port 3001");
+});
